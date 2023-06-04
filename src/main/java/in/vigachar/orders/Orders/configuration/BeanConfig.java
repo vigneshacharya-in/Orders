b7087e20ad7f37990bff0c6b8cc73fbc9b3c96b6
Vignesh Acharya <vignesh.achaarya@gmail.com>
2023-06-04 23:37:50 +0530

    Latest code

--- src/main/java/in/vigachar/orders/Orders/configuration/BeanConfig.java
+++ src/main/java/in/vigachar/orders/Orders/configuration/BeanConfig.java
@@ -1,13 +1,11 @@
 package in.vigachar.orders.Orders.configuration;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class ResilienceConfig {
+public class BeanConfig {
 
     @Bean
     public WebClient webClient() {
