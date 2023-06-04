b7e8aa0b511d7393812b579262171266785974f7
Vignesh Acharya <vignesh.achaarya@gmail.com>
2023-06-04 22:17:12 +0530

    Latest code

--- src/main/java/in/vigachar/orders/Orders/configuration/ResilienceConfig.java
+++ src/main/java/in/vigachar/orders/Orders/configuration/ResilienceConfig.java
@@ -4,13 +4,18 @@ import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ResilienceConfig {
-
     @Bean
     public CircuitBreakerRegistry circuitBreakerRegistry() {
         CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.ofDefaults();
         return CircuitBreakerRegistry.of(circuitBreakerConfig);
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create();
     }
 }
--- src/main/java/in/vigachar/orders/Orders/service/OrderService.java
+++ src/main/java/in/vigachar/orders/Orders/service/OrderService.java
@@ -7,20 +7,20 @@ import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import java.util.List;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final RestTemplate restTemplate;
+
+    private final WebClient webClient;
     private final io.github.resilience4j.circuitbreaker.CircuitBreaker circuitBreaker;
 
-
     @Autowired
-    public OrderService(OrderRepository orderRepository, RestTemplate restTemplate, CircuitBreakerRegistry circuitBreakerRegistry) {
+    public OrderService(OrderRepository orderRepository, WebClient webClient, CircuitBreakerRegistry circuitBreakerRegistry) {
         this.orderRepository = orderRepository;
-        this.restTemplate = restTemplate;
+        this.webClient = webClient;
         this.circuitBreaker = circuitBreakerRegistry.circuitBreaker("customers");
     }
 
@@ -29,9 +29,11 @@ public class OrderService {
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new OrderNotFoundException("Order not found with ID: " + orderId));
 
-        Customer customer = circuitBreaker.executeSupplier(() ->
-                restTemplate.getForObject("http://localhost:8081/api/v1/customers/" + order.getCustomerId(), Customer.class)
-        );
+        Customer customer = webClient.method(HttpMethod.GET)
+                .uri("http://localhost:8081/api/v1/customers/{customerId}", order.getCustomerId())
+                .retrieve()
+                .bodyToMono(Customer.class)
+                .block();
         order.setCustomer(customer);
 
         // Get the current state of the circuit breaker
@@ -51,12 +53,20 @@ public class OrderService {
     public Order fallbackCustomer(Long orderId, Exception ex) throws OrderNotFoundException {
         // Fallback logic when the circuit is open or an error occurs
         // You can return a default customer or an empty list of orders
+        io.github.resilience4j.circuitbreaker.CircuitBreaker.Metrics metrics = circuitBreaker.getMetrics();
+        System.out.println("Circuit Breaker Metrics:");
+        System.out.println("  - Successful calls: " + metrics.getNumberOfSuccessfulCalls());
+        System.out.println("  - Failed calls: " + metrics.getNumberOfFailedCalls());
+        System.out.println("  - Slow calls: " + metrics.getNumberOfSlowCalls());
+        System.out.println("  - Not permitted calls: " + metrics.getNumberOfNotPermittedCalls());
+        
         return orderRepository.findById(orderId)
                 .orElseThrow(() -> new OrderNotFoundException("Order not found with ID: " + orderId));
     }
 
     public class OrderNotFoundException extends Exception {
         public OrderNotFoundException(String s) {
+            super(s);
         }
     }
 }
