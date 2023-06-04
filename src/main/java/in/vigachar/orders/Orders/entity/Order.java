53ce7264c7260898d5631be4bc8b99f256fd0c01
Vignesh Acharya <vignesh.achaarya@gmail.com>
2023-06-04 20:40:49 +0530

    Initial commit

---
+++ src/main/java/in/vigachar/orders/Orders/entity/Order.java
@@ -0,0 +1,33 @@
+package in.vigachar.orders.Orders.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private Long customerId;
+
+    @Column(nullable = false)
+    private LocalDate orderDate;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(nullable = false)
+    private String status;
+}
+
---
+++ src/test/java/in/vigachar/orders/Orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vigachar.orders.Orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
